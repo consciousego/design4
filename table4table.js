affbc12a00b6450182eb8978a8d816193684855e
consciousego <33110870+consciousego@users.noreply.github.com>
2017-10-25 23:47:59 -0400

    Create table4table.js

---
+++ table4table.js
@@ -0,0 +1,36 @@
+/**
+	plugin: Pricing Table
+	version: 01
+	website: logodesign4.us
+	description: Pricing Table for Squarespace
+*/
+function initPricingTable(a) {
+  void 0 === a ? console.error("Pricing table parameter is undefined.") : 
+	(pricingTableFn(a), $(window).on("popstate", function (e, a) {
+		$("body div:last-child div:nth-child(4)").bind("DOMSubtreeModified", function () {
+			pricingTableFn(a), $(this).off("DOMSubtreeModified")
+		})
+	}), $("body").on("click", "a", function () {
+		$("body div:last-child div:nth-child(4)").bind("DOMSubtreeModified", function () {
+			pricingTableFn(a), $(this).off("DOMSubtreeModified")
+		})
+	}))
+}
+
+function pricingTableFn(item){
+	$('.' + item).hover(function() {
+		if ($(this).hasClass('default')) {
+			$('.default').addClass('active');
+		} else {
+			$(this).addClass("active");
+			$('.default').removeClass('active');
+		}
+	}, function() {
+		if ($(this).hasClass('default')) {
+			$(this).addClass("active");
+		} else {
+			$(this).removeClass("active");
+			$('.default').addClass('active');
+		}
+	});
+}
